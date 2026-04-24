a0bb98e40227cd234855992ca134ff926a9b9687
TanyaSoni2912 <tanu139sona@gmail.com>
2026-04-24 13:25:38 +0530

    checkks

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -16,19 +16,19 @@ class MenuItem extends Model
         'is_active'
     ];
 
-    // 🔗 Relation with Page
+    //  Relation with Page
     public function page()
     {
         return $this->belongsTo(Page::class);
     }
 
-    // 🔗 Parent Menu (for submenu)
+    //  Parent Menu (for submenu)
     public function parent()
     {
         return $this->belongsTo(MenuItem::class, 'parent_id');
     }
 
-    // 🔗 Child Menu (for submenu)
+    //  Child Menu (for submenu)
     public function children()
     {
         return $this->hasMany(MenuItem::class, 'parent_id')->orderBy('order');
